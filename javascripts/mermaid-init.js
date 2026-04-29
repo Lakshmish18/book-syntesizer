234d0656f46fcadb04aff1ea3c386a0594520913
Lakshmish18 <124867844+Lakshmish18@users.noreply.github.com>
2026-04-29 16:10:29 +0530

    Deployed 4c949a3 with MkDocs version: 1.6.1

--- javascripts/mermaid-init.js
+++ javascripts/mermaid-init.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function () {
+  if (window.mermaid) {
+    window.mermaid.initialize({ startOnLoad: true });
+  }
+});
 document$.subscribe(function () {
   if (typeof mermaid !== "undefined") {
     mermaid.initialize({
